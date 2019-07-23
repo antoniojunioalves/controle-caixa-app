3b1042c3d0fd978420ea9d1d14002e1ccbe4d2fe
antoniojunioalves <antoniojunioalves@gmail.com>
2019-07-23 17:02:02 -0300

    Alteração da revisão

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import * as serviceWorker from './serviceWorker';
 import './index.css';
 import App from '../src/components/App/App.js';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-serviceWorker.unregister();
