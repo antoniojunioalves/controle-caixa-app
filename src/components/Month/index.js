5553bb24a63ee119a8698e166c845108f4111eb8
antoniojunioalves <antoniojunioalves@gmail.com>
2019-08-14 23:41:42 -0300

    Melhorando o visual da aplicação

--- src/components/Month/index.js
+++ src/components/Month/index.js
@@ -3,36 +3,41 @@ import React from 'react'
 import './month.css'
 
 export default ({ month }) => {
+  const saldo = (month.totalCredito - month.totalDebito) < 0 ? 'month-line-totalizer negativo' : 'month-line-totalizer'
+
   return (
     <section className='month-main'>
       <div className='month-title' >{month.mes}</div>
 
-      <div className="month-div-line">
-        <div className='month-sub-title'></div>
-        <div className='month-sub-title'>Pagar</div>
-        <div className='month-sub-title'>Receber</div>
+      <div className="month-line">
+        {/* <button>Teste</button> */}
+        <div className='month-pag-rec'></div>
+        <div className='month-pag-rec'>Pagar</div>
+        <div className='month-pag-rec'>Receber</div>
       </div>
 
-      {month.parcelas.map(parcela => {
-        const credito = parcela.tipoLancamento === 'C'
-        const monthLine = credito ? 'month-line-revenue' : 'month-line-expense'
-        return (
-          <div key={parcela.descricao} className="month-div-line">
-            <div className={monthLine}>{parcela.descricao}</div>
-            <div className={monthLine}>{!credito ? parcela.valor : 0}</div>
-            <div className={monthLine}>{credito ? parcela.valor : 0}</div>
-          </div>
-        )
-      })}
+      <div className="meses">
+        {month.parcelas.map(parcela => {
+          const credito = parcela.tipoLancamento === 'C'
+          const monthLine = credito ? 'month-line-revenue' : 'month-line-expense'
+          return (
+            <div key={parcela.descricao} className="month-line">
+              <div className={monthLine}>{parcela.descricao}</div>
+              <div className={monthLine}>{!credito ? parcela.valor : 0}</div>
+              <div className={monthLine}>{credito ? parcela.valor : 0}</div>
+            </div>
+          )
+        })}
+      </div>
 
-      <div className="month-div-line">
+      <div className="month-line ultimo">
         <div className='month-line-totalizer'>Totalizador</div>
         <div className='month-line-totalizer'>{month.totalDebito}</div>
         <div className='month-line-totalizer'>{month.totalCredito}</div>
 
-        <div className='month-line-totalizer'></div>
-        <div className='month-line-totalizer'></div>
-        <div className='month-line-totalizer'>R$: {month.totalCredito - month.totalDebito}</div>
+        <div className='month-line-totalizer'>-</div>
+        <div className='month-line-totalizer'>Saldo</div>
+        <div className={saldo}>R$: {month.totalCredito - month.totalDebito}</div>
       </div>
     </section>
   )
