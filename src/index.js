cc1e9beb4945f896fbbb5016be0ce8f1e128f430
antoniojunioalves <antoniojunioalves@gmail.com>
2019-09-04 16:00:29 -0300

    Update index.js
    Correção do compose

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 
